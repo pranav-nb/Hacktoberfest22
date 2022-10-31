07b156376f61f71539ad7e10c077c0ce07dddb5a
ark <andrewstark013@gmail.com>
2022-10-31 19:19:14 +0530

    work added

---
+++ Java/Affirmation/app/src/androidTest/java/com/example/affirmation/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.example.affirmation.data
+
+import com.example.affirmation.R
+import com.example.affirmation.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations():List<Affirmation>{
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+      }
+}
